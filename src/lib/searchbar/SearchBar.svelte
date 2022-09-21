d5fd3446216f6f02e399433ca843c36799dce8c9
Alexandra Ferdinand <aferdina@redhat.com>
2022-09-21 15:28:05 -0400

    add typeahead searchbar

---
+++ src/lib/searchbar/SearchBar.svelte
@@ -0,0 +1,50 @@
+<script>
+import Typeahead from "svelte-typeahead";
+
+  const data = [
+    {
+      recipe: 'noods'
+    },
+    {
+      recipe: 'foods'
+    },
+    {
+      recipe: 'biscuit'
+    },
+    {
+      recipe: 'cookie'
+    },
+    {
+      recipe: 'chocolate cookie'
+    },
+    {
+      recipe: 'pasta'
+    },
+    {
+      recipe: 'beef'
+    }
+  ];
+
+  const extract = (/** @type {{ recipe: string; }} */ item) => item.recipe
+
+  /**
+	 * @type {any[]}
+	 */
+  let recipes = [];
+
+  </script>
+  
+  <Typeahead 
+  placeholder={`search for something`} 
+  inputAfterSelect={'clear'}
+  label="Search Recipes" 
+  {data} 
+  extract={(item) => item.recipe}
+  hideLabel 
+  limit={10} 
+  />
+
+  <style>
+  :global([role="listbox"])
+  { margin: 0; }
+  </style>
