fa12df863bb4c330e833aa54824eda767e3c7ed6
Ryan Long <rlong@redhat.com>
2022-09-22 10:52:55 -0400

    add way to edit generated recipe (#18)
    * add way to edit generated recipe
    
    * start submit logic

--- src/routes/builder/your-recipe.svelte
+++ src/routes/builder/your-recipe.svelte
@@ -1,6 +1,12 @@
 <script context="module" lang="ts">
   import Recipe from '$lib/recipes/Recipe.svelte';
+  import Button from '$lib/button/Button.svelte';
   import { currentBuilderStatus } from '$lib/stores.js';
+  import { goto } from '$app/navigation';
+
+  const submitRecipe = () => {
+    console.log('yay');
+  };
 </script>
 
 <svelte:head>
@@ -12,6 +18,8 @@
   <!-- TODO Fix this -->
   {#if $currentBuilderStatus.name}
     <Recipe data={$currentBuilderStatus} />
+    <Button variant="accent" on:click={submitRecipe}>Submit</Button>
+    <Button on:click={() => goto('/builder')}>Edit</Button>
   {:else}
     <span> no data </span>
   {/if}
