1048304175b9c102c36da0cbaadd08fb442ee86b
Ryan Long <rlong@redhat.com>
2022-09-23 11:11:32 -0400

    add recipe list

--- src/lib/recipes/RecipeList.svelte
+++ src/lib/recipes/RecipeList.svelte
@@ -1,13 +1,33 @@
-<script>
-  // import entire JSON file as a default import
-  import { dishes } from '$lib/data/all.json';
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { API_BASE_URL } from '$lib/consts';
+  import { recipeList } from '../stores';
+
+  const getRecipeURL = API_BASE_URL + '/recipes';
+
+  onMount(async () => {
+    fetch(getRecipeURL)
+      .then((response) => response.json())
+      .then((data) => {
+        recipeList.set(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        return [];
+      });
+  });
 </script>
 
 <div class="recipeList">
   <h1>List of all recipes by category</h1>
-  {#each dishes as dish}
-    <a href={`recipes/${dish}`}> {dish} </a>
-  {/each}
+
+  {#if $recipeList.length}
+    {#each $recipeList as recipe}
+      <a href={`recipes/${recipe.name}`}> {recipe.name} </a>
+    {/each}
+  {:else}
+    <span> Loading </span>
+  {/if}
 </div>
 
 <style>
--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,8 +1,10 @@
 import { writable } from 'svelte/store';
+import type { RecipeListInterface } from './responseInterface';
 
 // Recipe Stuff
 import * as emptyRecipe from '$lib/data/empty.json';
 export const currentBuilderStatus = writable(emptyRecipe);
+export const recipeList = writable([] as RecipeListInterface[]);
 
 // Authentication
 export const isAuthenticated = writable(false);
